04182969065c40eb127d7f4d4fb8caafab78bc23
Tanay Kumar Bera <tanaykumarbera@gmail.com>
2026-03-25 07:50:56 +0530

    feat: initialize Gradle project skeleton with version catalog

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolution {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "compose-electric-pop"
+
+include(":library")
+include(":demo")
